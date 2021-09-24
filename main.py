49b19173339faeb5173a905cee92dbfa2fd93711
Alessandra Boccuto <aless97b@gmail.com>
2021-09-24 18:08:20 +0200

    add pipeline

--- main.py
+++ main.py
@@ -2,12 +2,42 @@
 # File loaded and pickled in 7.5567114869753516 seconds
 # File unpickled in 1.1141046166419983 minutes
 
-import dataset_handler
-
+import os
+import const
+import dataset_handler as dh
 
 def main():
-    review_df = dataset_handler.initDataset('review')
-    dataset_handler.analyze(review_df, 'review')
+    review_dataset_tasks()
+   
+
+def review_dataset_tasks():
+    '''
+        Task 1, 2 and 3 that uses the review dataset.
+    '''
+
+    review_df = None
+
+    # check existence of balanced dataset versions, if not, create them
+    balanced_data_files = ['./balanced_dataset/dataset_sentiment.csv']
+    
+    if not all([os.path.exists(file) for file in balanced_data_files]) :
+        review_df = dh.initDataset('review')
+        dh.analyze(review_df, 'review')
+
+    task1_pipeline(review_df)
+
+    # task2_pipeline(review_df)
+
+
+def task1_pipeline(data):
+    # get a dataset sentiment balanced, if it already exists load from csv
+    balanced_df = dh.get_balanced_subset(data, 'sentiment')
+    
+    # pipeline
+    test_data = balanced_df.head(100)
+
+
+
 
 if __name__ == "__main__":
     main()
