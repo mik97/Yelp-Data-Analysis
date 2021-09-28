880180f4536d5832c8a3488cf2ad23a727250c7b
Michele Celozzi <michele.celozzi97@gmail.com>
2021-09-28 12:54:38 +0200

    Added get word embedding

--- main.py
+++ main.py
@@ -3,6 +3,7 @@
 # File unpickled in 1.1141046166419983 minutes
 
 import os
+import pickle
 
 import pandas as pd
 import dataset_handler as dh
@@ -13,7 +14,7 @@ from time import time
 
 def main():
     review_dataset_tasks()
-   
+
 
 def review_dataset_tasks():
     '''
@@ -23,11 +24,11 @@ def review_dataset_tasks():
     review_df = None
 
     # check existence of balanced dataset versions, if not, create them
-    balanced_data_files = ['./balanced_dataset/dataset_sentiment.csv']
-    
-    if not all([os.path.exists(file) for file in balanced_data_files]) :
-        review_df = dh.initDataset('review')
-        dh.analyze(review_df, 'review')
+    # balanced_data_files = ['./balanced_dataset/dataset_sentiment.csv']
+
+    # if not all([os.path.exists(file) for file in balanced_data_files]) :
+    #     review_df = dh.initDataset('review')
+    #     dh.analyze(review_df, 'review')
 
     task1_pipeline(review_df)
     # task2_pipeline(review_df)
@@ -37,13 +38,16 @@ def task1_pipeline(data):
     # get a dataset sentiment balanced, if it already exists load from csv
 
     # balanced_df = dh.get_balanced_subset(data, 'sentiment')
-    
+
     # pipeline
     df = pd.read_csv("./test.csv")
-    
+
     # pass only the series with text
-    stuff = preprocessing.text_pipeline(df['text'])
-   
+    cleanedSentences = preprocessing.text_pipeline(df['text'])
+
+    # get word embedding of reviews
+    wordEmbedding = preprocessing.get_word_embedding(cleanedSentences)
+    # print(wordEmbedding)
 
 
 if __name__ == "__main__":
