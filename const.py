567b641c7a4557124203ae38252ae864e34598bd
Alessandra Boccuto <aless97b@gmail.com>
2021-11-16 18:09:39 +0100

    moved data

--- const.py
+++ const.py
@@ -3,9 +3,11 @@ dataset_path = "../yelp dataset/"
 
 # path for pickled file
 pkl_path = "./pickled_data/"
-balanced_csv_path = "./balanced_dataset/"
 plots_path = "./plots/"
 w2v_path = "./w2v_models/"
+
+# contains stuff like balanced_review_sentiment.csv, review_sentiment_train.csv, review_sentiment_test.csv
+data_csv_path = "./data/"
 
 # random state
 seed = 42
