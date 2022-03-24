b267a5495c6fa39f56cb704ebe790587111e74bd
Alessandra Boccuto <aless97b@gmail.com>
2022-03-24 19:45:04 +0100

    remove pickled

--- libraries/filenames_generator.py
+++ libraries/filenames_generator.py
@@ -1,8 +1,5 @@
 import constants as const
 
-import pickle
-import time
-
 
 def embedding_matrix_pkl_file_name(name):
     return f'{const.embedding_path}{name}_embedding_matrix.npy'
@@ -26,17 +23,3 @@ def pickled_tokens(set, task):
 
 def picked_cleaned_sentences(set, task):
     return f'{const.pickled_path}{task}_{set}_cleaned_sentences.pkl'
-
-
-def get_minutes(start_time):
-    return round(((time.time() - start_time)/60), 4)
-
-
-def save_pickled(path, data):
-    with open(path, 'wb') as f:
-        pickle.dump(data, f)
-
-
-def load_pickled(path):
-    with open(path, 'rb') as f:
-        return pickle.load(f)
